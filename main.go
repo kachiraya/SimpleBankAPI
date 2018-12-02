3c5d4a7138d96eb02608a8c38e34a6f2525f796f
Achiraya Klinpipat (Layle) <achiraya.klinpipat@gmail.com>
2018-12-02 19:49:14 +0700

    --no-edit

--- main.go
+++ main.go
@@ -15,5 +15,28 @@ func main() {
 		log.Fatal(err)
 	}
 
+	createTable := `
+	CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        first_name TEXT,
+        last_name TEXT
+	);
+	`
+	if _, err := db.Exec(createTable); err != nil {
+		log.Fatal(err)
+	}
+	createTable = `
+	CREATE TABLE IF NOT EXISTS bankaccounts (
+        id SERIAL PRIMARY KEY,
+        userid SERIAL,
+        account_number SERIAL,
+        name TEXT,
+        balance NUMBER
+	);
+	`
+	if _, err := db.Exec(createTable); err != nil {
+		log.Fatal(err)
+	}
+
 	user.StartServer(":"+os.Getenv("PORT"), db)
 }
